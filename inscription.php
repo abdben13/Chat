9ca071f38eea9130701eca388795f0b640034d2d
abdel <abdelaziz.bensenouci2023@campus-eni.fr>
2024-01-18 15:32:28 +0100

    sauvegarde18/01

--- inscription.php
+++ inscription.php
@@ -9,8 +9,12 @@
 </head>
 
 <body>
-
-    <form action="" class="form_connexion_inscription">
+    <?php 
+        if(isset($_POST['button_inscription'])){
+            echo "Formulaire envoyé";
+        }
+    ?>
+    <form action="" method="POST" class="form_connexion_inscription" >
         <h1>INSCRIPTION</h1>
         <p class="message_error"></p>
         <label>Adresse Mail</label>
@@ -19,7 +23,7 @@
         <input type="password" name="mdp1" class="mdp1">
         <label>Confirmation du mot de passe</label>
         <input type="password" name="mdp2" class="mdp2">
-        <input type="submit" value="Inscription">
+        <input type="submit" value="Inscription" name="button_inscription">
         <p class="link">Vous avez un compte?<a href="index.php"> Se connecter</a></p>
     </form>
 
